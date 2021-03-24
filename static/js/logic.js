799da5d7a34e88f384801ba5d4babe42e560d057
maylacdao <may.lacdao@gmail.com>
2021-03-24 00:52:06 -0400

    Updates

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,8 +1,38 @@
-const url = "api/stock"
+const url = ("/api/stock")
 
 // Load data
 
 d3.json(url).then(function(data){
     console.log(data);
 });
+
+var dates = data.map(data => data.date);
+var closingPrices = data.map(data => data.closingPrice);
+
+var trace = {
+  type: "scatter",
+  mode: "lines",
+  x: dates,
+  y: closingPrices,
+  line: {
+    color: "#17BECF"
+      }
+};
+
+var data = [trace];
+
+var layout = {
+      title: `WMT closing prices`,
+      xaxis: {
+        range: [startDate, endDate],
+        type: "date"
+      },
+      yaxis: {
+        autorange: true,
+        type: "linear"
+      }
+    };
+
+Plotly.newPlot("plot", data, layout)
+
 
