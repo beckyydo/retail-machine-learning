06dd0c128188cea76e13214faa58b5e196cdf5f8
maylacdao <may.lacdao@gmail.com>
2021-03-24 17:56:37 -0400

    Updates

--- actualApp/app.py
+++ actualApp/app.py
@@ -35,6 +35,8 @@ walmart = Base.classes.walmart
 comparison = Base.classes.comparison
 predictions = Base.classes.predictions
 stock = Base.classes.stock2020
+prophet = Base.classes.prophet
+stockforecast = Base.classes.stockforecast
 
 session = Session(engine)
 
@@ -226,6 +228,110 @@ def stock_route():
         
     return jsonify(stock_df)
 
+# ****************************************Time Series Forecast with FB Prophet Route ****************************************
+@app.route("/api/prophet")
+def prophet_route():
+    data = session.query(prophet.ds, 
+        prophet.trend,
+        prophet.yhat_lower,
+        prophet.yhat_upper,
+        prophet.trend_lower,
+        prophet.trend_upper,
+        prophet.additive_terms,
+        prophet.additive_terms_lower,
+        prophet.additive_terms_upper,
+        prophet.daily,
+        prophet.daily_lower,
+        prophet.daily_upper,
+        prophet.weekly,
+        prophet.weekly_lower,
+        prophet.weekly_upper,
+        prophet.yearly,
+        prophet.yearly_lower,
+        prophet.yearly_upper,
+        prophet.yhat,
+        prophet.y).all()
+ 
+    prophet_df = []
+    for row in data:
+        output={
+             'ds':row[0],
+            'trend':row[1],
+            'yhat_lower':row[2],
+            'yhat_upper':row[3],
+            'trend_lower':row[4],
+            'trend_upper':row[5],
+            'additive_terms':row[6],
+            'additive_terms_lower':row[7],
+            'additive_terms_upper':row[8],
+            'daily':row[9],
+            'daily_lower':row[10],
+            'daily_upper':row[11],
+            'weekly':row[12],
+            'weekly_lower':row[13],
+            'weekly_upper':row[14],
+            'yearly':row[15],
+            'yearly_lower':row[16],
+            'yearly_upper':row[17],
+            'yhat':row[18],
+            'y':row[19]
+        }
+        prophet_df.append(output)
+
+    return jsonify(prophet_df)
+
+
+@app.route("/api/stockforecast")
+def stockforecast_route():
+    data = session.query(stockforecast.ds,
+                    stockforecast.trend,
+                    stockforecast.yhat_lower,
+                    stockforecast.yhat_upper,
+                    stockforecast.trend_lower,
+                    stockforecast.trend_upper,
+                    stockforecast.additive_terms,
+                    stockforecast.additive_terms_lower,
+                    stockforecast.additive_terms_upper,
+                    stockforecast.daily,
+                    stockforecast.daily_lower,
+                    stockforecast.daily_upper,
+                    stockforecast.weekly,
+                    stockforecast.weekly_lower,
+                    stockforecast.weekly_upper,
+                    stockforecast.yearly,
+                    stockforecast.yearly_lower,
+                    stockforecast.yearly_upper,
+                    stockforecast.yhat).all()
+
+    stockforecast_df = []
+    for row in data:
+        output = {
+            'ds':row[0],
+            'trend':row[1],
+            'yhat_lower':row[2],
+            'yhat_upper':row[3],
+            'trend_lower':row[4],
+            'trend_upper':row[5],
+            'additive_terms':row[6],
+            'additive_terms_lower':row[7],
+            'additive_terms_upper':row[8],
+            'daily':row[9],
+            'daily_lower':row[10],
+            'daily_upper':row[11],
+            'weekly':row[12],
+            'weekly_lower':row[13],
+            'weekly_upper':row[14],
+            'yearly':row[15],
+            'yearly_lower':row[16],
+            'yearly_upper':row[17],
+            'multiplicative_terms':row[18],
+            'multiplicative_terms_lower':row[19],
+            'multiplicative_terms_upper':row[20],
+            'yhat':row[21]
+        }
+        stockforecast_df.append(output)
+
+    return jsonify(stockforecast_df)
 
 if __name__ == "__main__":
     app.run()
