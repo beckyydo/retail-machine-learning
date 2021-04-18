2232bd54280c62e56e34b7386e8343d6e3b22217
beckyydo <47190773+beckyydo@users.noreply.github.com>
2021-04-18 01:49:50 -0400

    metric card

--- actualApp - Copy/app.py
+++ actualApp - Copy/app.py
@@ -64,7 +64,7 @@ session = Session(engine)
 def overview_metric():
     # Aggregate last 2 weeks of sales
     query = session.query(walmart.Week_Date, func.sum(walmart.Weekly_Sales))\
-                        .group_by(walmart.Week_Date).order_by(walmart.Week_Date.desc()).limit(2)  
+                        .group_by(walmart.Week_Date).order_by(walmart.Week_Date.desc()).limit(3)  
     # Convert query into dataframe 
     sls_2wks = pd.read_sql(query.statement, query.session.bind)
     # Calculate increase/decrease percentage of most recent weekly sale
@@ -73,8 +73,15 @@ def overview_metric():
     last_wk = sls_2wks.iloc[0,0]
     # Retrieve recent total sales
     last_sales = sls_2wks.iloc[0,1]
+
+    # Second last week calculation
+    # Margin
+    margin2 = (sls_2wks.iloc[1,1]-sls_2wks.iloc[2,1])/sls_2wks.iloc[2,1]*100
+    # Retrieve recent total sales
+    last_sales2 = sls_2wks.iloc[1,1]
     # Overview Metric Dict.
-    metric = ({'Margin': margin, 'Week': last_wk, 'Sales':last_sales})
+    metric = ({'Margin': round(margin,2), 'Week': last_wk, 'Sales': round(last_sales,2), 
+                'Diff_Margin': margin-margin2, 'Diff_Sales': last_sales-last_sales2})
     return jsonify(metric)
 
 
@@ -122,7 +129,100 @@ def share():
 @app.route("/storelocation")
 def store_location():
     return render_template("store.html") 
- 
+
+
+
+# **************************************** Grocery List Recommendation Service Route ****************************************
+grocery_list = []
+feature_list = []
+# Grocery List Recommendation
+def recommendations(user_email):
+    email = str(user_email)
+    
+    # Convert e-mail to user_id
+    user_id = int(user_df.loc[user_df['email'] == email, 'user_id'])
+    
+    # Grab past order from 
+    order = orders[orders['user_id'] == user_id].sort_values('add_to_cart_order')
+    # Spilt repeat orders and non-repeat orders
+    repeat = order[order['reordered'] > 0]
+    nonrepeat = order[order['reordered'] == 0]
+    
+    # Grab user past orders in kmean prediction format
+    grocery_df = pd.read_sql_table("grocery_df", conn)
+    user_order = grocery_df[grocery_df['user_id'] == user_id].drop('user_id', axis = 1)
+    
+    # Fit user_id on model, return cluster 
+    cluster_num = kmeans.predict(user_order.to_numpy())[0]
+    top10 = cluster_top10[cluster_top10['cluster'] == cluster_num]
+    
+    # Set starting variables
+    n = 0
+    for product in repeat['product_name']:
+        top10_check = top10[top10['product_name'] == product]
+        if (n == 3):
+            break
+        elif (not top10_check.empty):
+            url_list = img_product.loc[img_product['product'] == product].img_url.item()
+            grocery_list.append({'product': product, 'img': url_list})
+            n = n + 1
+
+    for product in nonrepeat['product_name']:
+        nonrepeat_check = nonrepeat[nonrepeat['product_name'] == product]
+        if (n == 3):
+            break
+        elif (not nonrepeat_check.empty):
+            url_list = img_product.loc[img_product['product'] == product].img_url.item()
+            grocery_list.append({'product': product, 'img': url_list})
+            n = n + 1
+
+    for product in repeat['product_name']:
+        if (n == 3):
+            break
+        else:
+            url_list = img_product.loc[img_product['product'] == product].img_url.item()
+            grocery_list.append({'product': product, 'img': url_list})
+            n = n + 1
+#    return grocery_list
+
+def feature_2(user_email, user_df):
+    email = str(user_email)
+    # Convert e-mail to user_id
+    user_id = int(user_df.loc[user_df['email'] == email, 'user_id'])
+    # Filter recommendation dataframe
+    user_df = feature2[feature2['user_id'] == user_id]
+    
+    k=0
+    grocery_check = [item['product'] for item in grocery_list]
+    for product in user_df['product_name']:
+        if (k==3):
+            break
+        elif product not in grocery_check:
+            url_list = img_product.loc[img_product['product'] == product].img_url.item()
+            feature_list.append({'product':product,'img':url_list})
+            k=k+1
+#return feature_list
+
+# Main route to render index.html
+@app.route("/login")
+def login_page():
+    return render_template("login.html")
+
+@app.route("/recommendations", methods = ['POST'])
+def grocery():
+    # Retrieve e-mail from login
+    user_email = request.form['user_email']
+    # Call function
+    recommendations(user_email)
+    feature_2(user_email, user_df)
+    # Render Landing Page
+    return render_template("landing.html", grocery_list = grocery_list, feature_list = feature_list)
+
+@app.route("/cart")
+def shopping_cart():
+    return render_template('cart.html', grocery_list = grocery_list,feature_list = feature_list)
+
+
 
 if __name__ == "__main__":
     app.run()
--- actualApp - Copy/static/js/app.js
+++ actualApp - Copy/static/js/app.js
@@ -94,193 +94,48 @@ searchMobileTrigger.addEventListener('click', () => {
 });
 
 /*****************************************OVERVIEW*****************************************/
+function diff_arrow(num, d3_element){
+    var dollar = d3.format("$,.2f")(num)
+    if (num > 0) {
+        d3_element.html('<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-up" fill="green" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"/></svg><span style="color:green;">'
+        + dollar +"</span>")
+    } else{
+        d3_element.html('<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-down" fill="red" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"/></svg><span style="color:red;">'
+        + dollar+"</span>");
+    }
+};
+
 /*METRIC*/
 d3.json("/overview/metric").then( weekdata =>{
-	console.log(weekdata)
+    console.log(weekdata)
+    var sale = weekdata.Sales
+    var week = weekdata.Week
+    var margin = weekdata.Margin
+
+    /*Sales*/
+    var dollar_format = d3.format("$,.2f")
+    var sale_format = dollar_format(sale)
+    var metric_sales = d3.selectAll("#overview_sales")
+    metric_sales.text(sale_format);
+    
+    /*Difference in sales last week*/
+    var diff_sales = d3.selectAll("#diff_sales")
+    var sales_diff = weekdata.Diff_Sales
+    diff_arrow(sales_diff,diff_sales)
+
+    /*Margin*/
+    var metric_margin = d3.selectAll("#overview_margin")
+    metric_margin.text(margin + "%")
+    /*Difference in sales last week*/
+    var diff_margin = d3.selectAll("#diff_margin")
+    var margin_diff = weekdata.Diff_Margin
+    diff_arrow(margin_diff,diff_margin)
+
+    var formatDate = d3.timeFormat("%B %d,%Y")
+    console.log(date_format)
+    var date_format = formatDate(new Date(week))
+    var metric_week = d3.selectAll("#overview_week")
+    metric_week.text(date_format)
+ 
 });
 
-// ******************************** MARKET SHARE ********************************
-// Market Share Service Route
-var url = "/api/marketshare"
-
-// Initalize Graph and Tables
-d3.json(url).then(data =>{
-    // Retrieve State Name List
-    var state_name = data.map(d => d.State);
-    // Add All Option in Menu
-    state_name.push("*All")
-    // Sort and Get Unique State Name for Dropdown Menu
-    var unique_state = [...new Set(state_name.sort())]; 
-    // Dropdown Menu
-    var DropDownMenu = d3.select("#marketselDataset");
-    // Remove old html id names
-    DropDownMenu.html("");    
-    // Append id names option to html
-    unique_state.map(id_name => DropDownMenu.append("option").attr("value",id_name).html(id_name));
-    // Initalize Data 
-    share_inital(data);
-})
-    
-function share_inital(data){
-    
-    // Set Colorscale
-    var scl = [[0,'rgb(5, 10, 172)'],[0.35,'rgb(40, 60, 190)'],[0.5,'rgb(70, 100, 245)'], [0.6,'rgb(90, 120, 245)'],[0.7,'rgb(106, 137, 247)'],[1,'rgb(220, 220, 220)']];
-    
-        var data1 = [{
-            type:'scattergeo',
-            locationmode: 'USA-states',
-            lon: data.map(d => d.Lon),
-            lat: data.map(d => d.Lat),
-            hoverinfor:  `City Name: ${data.map(d => d.City)} <br> Population: ${data.map(d=>d.Population)}`,
-            text:  data.map(d => d.City),
-            mode: 'markers',
-            marker: {
-                size: 8,
-                opacity: 0.8,
-                reversescale: true,
-                autocolorscale: false,
-                symbol: 'circle',
-                line: {
-                    width: 1,
-                    color: 'rgb(102,102,102)'
-                },
-                colorscale: scl,
-                cmin: d3.min(data,d=>d.Share),
-                cmax: d3.max(data,d=>d.Share),
-                color: data.map(d => d.Share),
-                colorbar: {
-                    title: 'Market Share (%) <br>(All)'
-                }
-            }
-        }];
-    
-    
-        var layout1 = {
-            title: 'Walmart Marketshare Over US (%)',
-            colorbar: true,
-            geo: {
-                scope: 'usa',
-                projection: {
-                    type: 'albers usa'
-                },
-                showland: true,
-                landcolor: 'rgb(250,250,250)',
-                subunitcolor: 'rgb(217,217,217)',
-                countrycolor: 'rgb(217,217,217)',
-                countrywidth: 0.5,
-                subunitwidth: 0.5
-            }
-        };
-    
-    Plotly.newPlot("market-share", data1, layout1);
-    
-    // Create table
-    var tbody = d3.select("tbody");
-    // Remove old tbody
-    tbody.html("");
-    //
-    tbody.attr()
-    
-    // Add data to tbody
-    data.forEach((entry)=>{
-        // Apend row
-        var row = tbody.append("tr");
-        // Append value to row
-        var cell = row.append("td");
-        cell.text(entry.City)
-        var cell2 = row.append("td");
-        cell2.text(entry.State)
-        var cell3 = row.append("td");
-        cell3.text(entry.Population)
-        var cell4 = row.append("td");
-        cell4.text(entry.Share)        
-    })
-};
-    
-// Update Plot
-d3.selectAll("#marketselDataset").on('change', updatePlotly);
-    
-// Update Plot Function
-function updatePlotly(){
-    
-    d3.json(url).then(data => {
-            // Get Value From Search Bar
-            var dropdownMenu = d3.select("#marketselDataset").node().value;
-            // Clear graphs
-            d3.select("#market-share").html("");
-    
-    
-            if (dropdownMenu == "*All"){
-                share_inital(data);
-            }
-            else {
-                var filter_data = data.filter(row => row.State == dropdownMenu);
-                var scl = [[0,'rgb(5, 10, 172)'],[0.35,'rgb(40, 60, 190)'],[0.5,'rgb(70, 100, 245)'], [0.6,'rgb(90, 120, 245)'],[0.7,'rgb(106, 137, 247)'],[1,'rgb(220, 220, 220)']];
-    
-                var data2 = [{
-                type:'scattergeo',
-                locationmode: 'USA-states',
-                lon: filter_data.map(d => d.Lon),
-                lat: filter_data.map(d => d.Lat),
-                hoverinfor:  filter_data.map(d => d.City),
-                text:  filter_data.map(d => d.City),
-                mode: 'markers',
-                marker: {
-                    size: 8,
-                    opacity: 0.8,
-                    reversescale: true,
-                    autocolorscale: false,
-                    symbol: 'circle',
-                    line: {
-                        width: 1,
-                        color: 'rgb(102,102,102)'
-                    },
-                    colorscale: scl,
-                    cmin: d3.min(data,d=>d.Share),
-                    cmax: d3.max(data,d=>d.Share),
-                    color: filter_data.map(d => d.Share),
-                    colorbar: {
-                        title: `Market Share (%)<br>(${dropdownMenu})`
-                    }
-                    }
-                }];
-        
-        
-                var layout2 = {
-                    title: `Walmart Marketshare Over US (%) <br>(${dropdownMenu})`,
-                    colorbar: true,
-                    geo: {
-                    scope: 'usa',
-                    projection: {
-                        type: 'albers usa'
-                    },
-                    showland: true,
-                    landcolor: 'rgb(250,250,250)',
-                    subunitcolor: 'rgb(217,217,217)',
-                    countrycolor: 'rgb(217,217,217)',
-                    countrywidth: 0.5,
-                    subunitwidth: 0.5}
-                };
-
-                Plotly.newPlot("market-share", data2, layout2);
-                // Create table
-                var tbody = d3.select("tbody");
-                // Clear tbody
-                tbody.html("");
-
-                filter_data.forEach((entry)=>{
-                    //Add row
-                    var row = tbody.append("tr");
-                    //Add value to row
-                    var cell = row.append("td");
-                    cell.text(entry.City)
-                    var cell2 = row.append("td");
-                    cell2.text(entry.State)
-                    var cell3 = row.append("td");
-                    cell3.text(entry.Population)
-                    var cell4 = row.append("td");
-                    cell4.text(entry.Share)        
-                });                
-            }
-    })
-};
