342d59c32f0f502385a4ee19c1694421104ca6f8
maylacdao <may.lacdao@gmail.com>
2021-04-21 17:44:59 -0400

    Updates

--- static/js/logicCSV.js
+++ static/js/logicCSV.js
@@ -1,103 +1,111 @@
-var xField = 'Date';
-var yfield = 'Adj Close';
-
-var selectorOptions = {
-    buttons: [{
-        step: 'month',
-        stepmode: 'backward',
-        count: 1,
-        label: '1m'
-    }, {
-        step: 'month',
-        stepmode: 'backward',
-        count: 6,
-        label: '6m'
-    }, {
-        step: 'year',
-        stepmode: 'todate',
-        count: 1,
-        label: 'YTD'
-    }, {
-        step: 'year',
-        stepmode: 'backward',
-        count: 1,
-        label: '1y'
-    }, {
-        step: 'all',
-    }],
-};
-
 d3.csv("/prophet_data.csv", function(data) {
     console.log(data);
 
-    var data2 = prepData(data);
-    var layout = {
-        title: 'Time series with range slider and selectors',
-        xaxis: {
-            rangeselector: selectorOptions,
-            rangeslider: {}
-        },
-        yaxis: {
-            fixedrange: true
-        }
-    };
+    var dates = [];
+    data.forEach(function(datapoint, i){
+        dates.push(new Date(datapoint[ds]));
+    })
 
-    Plotly.newPlot('plot-forecast', data2, layout);
+    var trace1 = {
+    fill: null,
+    mode:'markers',
+    name:'predicted price',
+    type:'scatter',
+    x: dates,
+    y: yhat
+  };
+
+  var trace2 = {
+    fill: 'tonexty',
+    line: {color:'#57b8ff'},
+    mode:'lines',
+    name: 'upper_band',
+    type:'scatter',
+    x: dates,
+    y: yhat_upper
+  };
+
+  var trace3 = {
+    fill: 'tonexty', 
+    line: {color: '#57b8ff'}, 
+    mode: 'lines', 
+    name: 'lower_band', 
+    type: 'scatter', 
+    x: dates,
+    y: yhat_lower
+  };
+
+  var trace4 = {
+    line: {color: '##ff6d22'}, 
+    mode: 'lines', 
+    name: 'model line of best fit', 
+    type: 'scatter', 
+    x: dates,
+    y: trend
+  };
+
+  var trace5 = {
+    fill: 'black',
+    mode:'markers',
+    name:'actual price',
+    type:'scatter',
+    x: dates,
+    y: actual
+  };
+
+  var traceData = [trace1,trace2,trace3,trace4,trace5];
+
+  // var selectorOptions = {
+  //   buttons: [{
+  //       step: 'month',
+  //       stepmode: 'backward',
+  //       count: 1,
+  //       label: '1m'
+  //   }, {
+  //       step: 'month',
+  //       stepmode: 'backward',
+  //       count: 6,
+  //       label: '6m'
+  //   }, {
+  //       step: 'year',
+  //       stepmode: 'todate',
+  //       count: 1,
+  //       label: 'YTD'
+  //   }, {
+  //       step: 'year',
+  //       stepmode: 'backward',
+  //       count: 1,
+  //       label: '1y'
+  //   }, {
+  //       step: 'all',
+  //   }],
+  // };
+
+  var layout = {
+    title: 'Walmart Stock Time Series Forecast', 
+    xaxis: {
+      title: 'Dates', 
+      ticklen: 5,
+      showticklabels: false, 
+      gridcolor: 'rgb(255, 255, 255)', 
+      gridwidth: 2, 
+      zerolinewidth: 1,
+      // rangeselector: selectorOptions,
+      rangeslider: {}
+      // type:'date'
+    }, 
+    yaxis: {
+      title: 'Price (USD)', 
+      ticklen: 5, 
+      gridcolor: 'rgb(255, 255, 255)', 
+      gridwidth: 2, 
+      zerolinewidth: 1,
+      fixedrange: true
+    }, 
+    plot_bgcolor: 'rgb(229, 236, 246)' 
+    // paper_bgcolor: 'rgb(243, 243, 243)'
+  };
+
+Plotly.newPlot('plot-forecast', traceData, layout);
 });
 
-function prepData(data2) {
-    var x = [];
-    var y = [];
-
-    data2.forEach(function(datum, i) {
-
-        x.push(new Date(datum[xField]));
-        y.push(datum[yField]);
-    });
-
-    return [{
-        mode: 'lines',
-        x: x,
-        y: y
-    }];
-}
-
-// var dates = []
-// dates.forEach(function(dataPoint) {
-//     dates.push(dataPoint.ds);
-//     console.log(dates);
-// });
-
-// var actualPrice = data.trend
-
-// var trace1 = {
-//     fill: null,
-//     mode: 'markers',
-//     name: 'Actual Stock Price',
-//     x: dates,
-//     y: actualPrice
-// };
-
-// var data = [trace1];
-
-// var layout = {
-//     title: 'WMT Time Series Forecast',
-//     xaxis: {
-//         title: 'Monthly Dates', 
-//         ticklen: 5, 
-//         gridcolor: 'rgb(255, 255, 255)', 
-//         gridwidth: 2, 
-//         zerolinewidth: 1
-//     }, 
-//     yaxis: {
-//         title: 'Pageviews', 
-//         ticklen: 5, 
-//         gridcolor: 'rgb(255, 255, 255)', 
-//         gridwidth: 2, 
-//         zerolinewidth: 1
-//     }, 
-//     plot_bgcolor: 'rgb(243, 243, 243)', 
-//     paper_bgcolor: 'rgb(243, 243, 243)'
-// };
-
-// Plotly.newPlot('plot', data, layout);
